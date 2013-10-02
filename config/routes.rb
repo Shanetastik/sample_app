e15169be886dba11fd69da9ac6abcdc9b7fbce55
Shane Lewis <lewis.shane1@gmail.com>
2013-10-01 19:39:44 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   match '/signup',  to: 'static_pages#signup',  via: 'get'
 
-  match '/home',    to: 'static_pages#home',    via: 'get'
+  root :to => 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
