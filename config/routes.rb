e6b30581651508f2224b5104a6f381c000d7b306
Shane Lewis <lewis.shane1@gmail.com>
2013-10-14 17:11:40 -0700

    Add SSL in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
-  root :to => 'static_pages#home'
+  root :to => 'static_pages#home',              via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
